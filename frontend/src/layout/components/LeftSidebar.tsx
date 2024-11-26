91fd2e248a8ceca435fc278afd1e66086d886c17
anand <anandvaibhaw000@gmail.com>
2024-11-27 00:12:02 +0530

    elaborated slide component

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -56,7 +56,26 @@ const LeftSidebar = () => {
 
         <Slide appear={false} direction="down" className="h-[calc(100vh-300px)]">
           <div className="space-y-2">
-            {isLoading ? "<PlaylistSkeleton/>" : "some music" }
+            {isLoading ? "<PlaylistSkeleton/>" : (
+              albums.map((album) => (
+                <Link to={`/albums${album._id}`}
+                key={album._id}
+                className="p-2 hover:bg-zinc-800 rounded-md flex items-center gap-3 group cursor-pointer"
+                >
+                   <img src={album.imageUrl} alt="Plalist img"
+                      className="size-12 rounded-md flex-shrink-0 object-cover"
+                   />
+                   <div className="flex-1 min-w-0 hidden md:block">
+                    <p className="font-medium truncate">
+                      {album.title}
+                    </p>
+                    <p className="text-sm text-zinc-400 truncate">
+                      Album + {album.artist}
+                    </p>
+                   </div>
+                </Link>
+              ))
+            ) }
           </div>
         </Slide>
       </div>
