1e98d1c8ef83bc8e69ec09732e3bb83786babf46
anand <anandvaibhaw000@gmail.com>
2024-11-26 22:30:42 +0530

    correction

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@mui/material";
 import { Outlet } from "react-router-dom"
+import LeftSidebar from "./components/LeftSidebar";
 
 const MainLayout = () => {
   // const isMobile = false;
@@ -8,7 +9,7 @@ const MainLayout = () => {
       <Container  className="flex-1 flex h-full overflow-hidden p-2">
         {/* left sidebar */}
         <Container maxWidth="sm">
-          left sidebar
+          <LeftSidebar />
         </Container>
 
         <Container maxWidth="sm" className="w-2 bg-black rounded-lg transition-colors">
--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -4,22 +4,23 @@ import { HomeIcon, Library, MessageCircle } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 const LeftSidebar = () => {
-  const isLoading = true;
+  // const isLoading = true;
 
   return (
     <div className="h-full flex flex-col gap-2">
-      LeftSidebar
+      {/* Navigation menu */}
+
       <div className="rounded-lg bg-zinc-900 p-4">
         <div className="space-y-2">
-        <Link
-              to={"/"}
-              className="w-full justify-start text-white hover:bg-zinc-800"
-            >
-              <HomeIcon className="mr-2 size-5" />
-              <span className="hidden md:inline">
-                Home
-              </span>
-            </Link>
+          <Link
+            to={"/"}
+            className="w-full justify-start text-white hover:bg-zinc-800"
+          >
+            <HomeIcon className="mr-2 size-5" />
+            <span className="hidden md:inline">
+              Home
+            </span>
+          </Link>
 
           <SignedIn>
             <Link
@@ -28,7 +29,7 @@ const LeftSidebar = () => {
             >
               <MessageCircle className="mr-2 size-5" />
               <span className="hidden md:inline">
-                Message
+                Messages
               </span>
             </Link>
           </SignedIn>
@@ -37,7 +38,7 @@ const LeftSidebar = () => {
 
       {/* Library section */}
       <div className="flex-1 rounded-lg bg-zinc-900 p-4">
-        <div className="flex items-cente justify-between mb-4">
+        <div className="flex items-center justify-between mb-4">
           <div className="flex items-center text-white px-2">
             <Library className="size-5 mr-2" />
             <span className="hidden md:inline">Playlists</span>
