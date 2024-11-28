c6ee5ab65eadad1ea527ad681acb3b0a56aaa107
anand <anandvaibhaw000@gmail.com>
2024-11-29 02:04:49 +0530

    built featuredGridSkeleton and sectionGrid

--- frontend/src/components/ui/Topbar.tsx
+++ frontend/src/components/ui/Topbar.tsx
@@ -1,28 +1,36 @@
-import { SignedIn, SignedOut, SignOutButton } from '@clerk/clerk-react';
+import { SignedOut, UserButton } from '@clerk/clerk-react';
 import { LayoutDashboardIcon } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import SignInOAuthButtons from './SignInOAuthButtons';
+import { useAuthStore } from '../../stores/useAuthStore';
 
 const Topbar = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuthStore();
+  console.log({ isAdmin });
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75">
       <div className="flex gap-2 items-center">
-        spotify
+        <img src="/spotify.png" alt="Spotify logo" className="size-8" />
+        Spotify
       </div>
       <div className="flex items-center gap-4">
         {isAdmin && (
-          <Link to={"/admin"}>
+          <Link 
+          to={"/admin"}
+          // className={cn(buttonVariants({ variant: "outline"}))}
+          >
             <LayoutDashboardIcon className="size-4 mr-2" />
             Admin Dashboard
           </Link>
         )}
-        <SignedIn>
+        {/* <SignedIn>
           <SignOutButton />
-        </SignedIn>
+        </SignedIn> */}
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
+
+        <UserButton />
       </div>
     </div>
   )
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,8 +1,28 @@
+import { useEffect } from 'react';
 import Topbar from '../../components/ui/Topbar'
+import { useMusicStore } from '../../stores/useMusicStore'
 
 const HomePage = () => {
+  const {
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  },[fetchFeaturedSongs,fetchMadeForYouSongs,fetchTrendingSongs]);
+
+  console.log({isLoading,madeForYouSongs,featuredSongs,trendingSongs});
+  
   return (
-    <div>
+    <div className="rounded-md overflow-hidden">
       <Topbar />
     </div>
   )
