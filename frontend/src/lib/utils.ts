97190824e6b1261e10d2e341d7ca019c289988b6
anand <anandvaibhaw000@gmail.com>
2024-12-03 23:49:44 +0530

    built utils

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,7 +1,6 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-
-const utils = () => {
-  
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
-
-export default utils
