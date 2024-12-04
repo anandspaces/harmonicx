f6a5e26a09d711eab543eb5022c078c2b3084708
anand <anandvaibhaw000@gmail.com>
2024-12-04 23:59:43 +0530

    imporved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 // import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 // import { Button } from '@mui/material'
 // import { axiosInstance } from './lib/axios'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/home/HomePage'
-import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage'
-import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react'
-import MainLayout from './layout/MainLayout'
-import ChatPage from './pages/chat/ChatPage'
-import AlbumPage from './pages/album/AlbumPage'
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage';
+import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react';
+import MainLayout from './layout/MainLayout';
+import ChatPage from './pages/chat/ChatPage';
+import AlbumPage from './pages/album/AlbumPage';
 
 export default function App() {
   // token
