85f09f5d3757faeb7795ddcc611c1f71d1171eea
anand <anandvaibhaw000@gmail.com>
2025-02-09 18:18:52 +0530

    modified

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://spotify-clone-backend-delta.vercel.app/api/",
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "http://localhost:5000/api",
 });
